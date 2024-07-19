f66d4fbec23f0cc488af6281f3ab01d46e43f781
YannKamche <yannkamche@gmail.com>
2024-07-19 14:43:38 +0100

    Finished Navbar, added images

---
+++ src/constants/links.ts
@@ -0,0 +1,28 @@
+const links = [
+    {
+        name: "Products",
+        href: "#"
+    },
+    {
+        name: "Pricing",
+        href: "#",
+    },
+    {
+        name: "Client",
+        href: "#",
+    },
+    {
+        name: "Resources",
+        href: "#",
+    },
+    {
+        name: "Documentation",
+        href: "#",
+    }, 
+    {
+        name: "Entreprise",
+        href: "#"
+    }
+]
+
+export default links
